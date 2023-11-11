12e3818652c061d00adbe3bbb7c7979d64598633
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-11-11 11:39:55 +0530

    added custom colors for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 const App = () => {
-  return <div className="text-red-700">App</div>;
+  return (
+    <section className="text-f-primary min-h-screen bg-default-gray">
+      <div className="w-64 h-56 bg-white"></div>
+    </section>
+  );
 };
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "default-gray": "#eff2f5",
+        "f-primary": "#0f1629",
+        "default-blue": "#0052fe",
+        "blue-btn-text": "#0141cf",
+        "green-btn": "#ebf9f4",
+        "green-btn-text": "#0fba83",
+        "orange-btn": "#ffb800",
+      },
+    },
   },
   plugins: [],
-}
+};
