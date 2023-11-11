875f55d9bb010b0d5e4c005baf166f872f5607ac
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-11-11 11:48:30 +0530

    added font Inter for project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <section className="text-f-primary min-h-screen bg-default-gray">
       <div className="w-64 h-56 bg-white"></div>
+      <h1>HElo KArthik</h1>
     </section>
   );
 };
