4797149b8307b6a7865fe8439d273b2239c4733c
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-11-13 15:53:04 +0530

    updated styles for faq

--- src/components/FaqContent.jsx
+++ src/components/FaqContent.jsx
@@ -3,12 +3,15 @@ import { faqs } from "../utils/faq.js";
 const FaqContent = () => {
   return (
     <div>
-      {faqs.map((faq) => {
+      {faqs.map((faq, index) => {
         const { question, answer } = faq;
         return (
           <div key={question} className="my-[15px]">
-            <h4 className="text-lg lg:text-xl font-bold">{question}</h4>
-            <p className="text-base leading-6 my-[15px]">{answer}</p>
+            <h4 className="text-lg lg:text-xl font-bold">
+              {index + 1}. {question}
+            </h4>
+            <p className="text-base leading-6 mt-[10px] lg:mb-3">{answer}</p>
+            <div className="bg-divider opacity-60 w-full h-[1px] my-[15px]"></div>
           </div>
         );
       })}
